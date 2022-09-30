0f0f5444cef049e07b978d01878ee23723339f49
H0rn3t Sp1d3rs <97798085+H0rn3t-Sp1d3rs@users.noreply.github.com>
2022-09-30 11:25:19 +0600

    Create bd.py

---
+++ bd.py
@@ -0,0 +1,61 @@
+#!bin/python
+import os
+os.system('clear')
+
+main='''\033[1;36;40m
+────────╔╗─╔╗───────╔╗
+────────║║─║║───────║║
+╔╗╔╦══╦═╝║─║╚═╦══╦══╣║╔╦══╦═╦══╗
+║╚╝║╔╗║╔╗║─║╔╗║╔╗║╔═╣╚╝╣║═╣╔╣══╣
+║║║║╔╗║╚╝╠╗║║║║╔╗║╚═╣╔╗╣║═╣║╠══║
+╚╩╩╩╝╚╩══╩╝╚╝╚╩╝╚╩══╩╝╚╩══╩╝╚══╝
+───────────────────╔╗
+──────────────────╔╝╚╗
+╔══╦══╦╗╔╦╗╔╦╗╔╦═╗╠╗╔╬╗─╔╗
+║╔═╣╔╗║╚╝║╚╝║║║║╔╗╬╣║║║─║║
+║╚═╣╚╝║║║║║║║╚╝║║║║║╚╣╚═╝║
+╚══╩══╩╩╩╩╩╩╩══╩╝╚╩╩═╩═╗╔╝
+─────────────────────╔═╝║
+─────────────────────╚══╝
+╯
+'''
+print("\033[1;34;40m=============================================")
+print("\033[1;33;40m|     DEVELOPED BY MAD HACKERS COMMUNITY    |")
+print("\033[1;34;40m=============================================")
+print(main)
+print("\033[1;34;40m=============================================")
+print("\033[1;33;40m|     DEVELOPED BY MAD HACKERS COMMUNITY    |")
+print("\033[1;34;40m=============================================")
+
+print("\033[1;33;40mCODE NAME: H0rn3t Sp1d3rs  ")
+print("\033[1;33;40mAuthor : Mad Hackers Community")
+print("GITHUB : HTTPS://GITHUB.COM/H0rn3t-Sp1d3rs")
+print("Website : https://www.h0rn3t-sp1d3rs.tk")
+
+print("\033[1;34;40m=============================================")
+print("\033[1;33;40m|     DEVELOPED BY MAD HACKERS COMMUNITY    |")
+print("\033[1;34;40m=============================================")
+print()
+print("\033[1;31;40m[\033[1;32;40m01\033[1;31;40m]\033[1;32;40m Termux Setup : ")
+
+print("\033[1;31;40m[\033[1;32;40m02\033[1;31;40m]\033[1;32;40m CONTACT OWNER :")
+
+opton=input("\033[1;36;40m[##] SELECT YOUR OPTION ==> \033[1;32;40m")
+os.system('clear')
+if opton=='01' or opton=='1':
+	os.system('pkg install git')
+	os.system('pkg install python')
+	os.system('pkg install python2')
+	os.system('pkg install php')
+	os.system('pip2 install requests')
+	os.system('pip install requests')
+	
+
+elif opton=='2' or opton=='02':
+	os.system(" xdg-open https://www.facebook.com/H0rn3t.Sp1d3rs")
+	os.system('python bd.py')
+
+
+else:
+	print('\033[1;31;40mWRONG')
+ 
